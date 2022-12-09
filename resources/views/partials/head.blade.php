15b6af8a2bbb893158b3b13075d8afd6de839efe
Ro8ch <53278837+Ro8ch@users.noreply.github.com>
2022-12-09 12:20:13 +0200

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -18,5 +18,5 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('images/logo.ico') }}" rel="shortcut icon" >
+    <link href="{{ asset('storage/images/logo.ico') }}" rel="shortcut icon" >
 </head>
