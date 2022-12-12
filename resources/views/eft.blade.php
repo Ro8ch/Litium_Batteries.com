7b33c75d1cda7767de8e09918a9e027efc3c81fd
Ro8ch <53278837+Ro8ch@users.noreply.github.com>
2022-12-12 12:39:58 +0200

    update

--- resources/views/eft.blade.php
+++ resources/views/eft.blade.php
@@ -8,11 +8,11 @@
     <div class = "cart-font-color">
         <H5><strong>Bank details:</H5></strong>
         <p></p>
-        <p>Bank:                    Nedbank</p>
-        <p>Account type:            Current account</p>
-        <p>Branch Code:             198765</p>
-        <p>Account holder:          Chanro du Toit</p>
-        <p>Account number:          1233400142</p>
+        <p>Bank:                    </p>
+        <p>Account type:            </p>
+        <p>Branch Code:             </p>
+        <p>Account holder:          </p>
+        <p>Account number:          </p>
         <p>Reference:               {{ format($OrderNumber) }}</p>
         <p>Amount:                  R{{ format($total) }}</p>
         <p></p>
